281b5643dab6faed1623aeb765b7208866910d70
Andrew Lee <andrewimg@gmail.com>
2020-11-13 11:10:27 -0800

    update

--- php/form-to-email.php
+++ php/form-to-email.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
 $subject = $_POST['subject'];
@@ -11,13 +12,18 @@ $email_subject = "Client Inquiry";
 
 $email_body = "You have received a new message from the user $firstName $lastName.\n".
                 "Here is the subject: $subject\n".
-                "Here is the message:\n $message";
+                "Here is the message:\n $message".
 
 $to = "andrewimg@gmail.com";
 
 $headers = "From: $email_from \r\n";
               
 $headers .= "Reply-To: $visitor_email \r\n";
-              
-mail($to,$email_subject,$email_body,$headers);
+
+
+if (mail($to,$email_subject,$email_body,$headers)) {
+    echo "Mail Sent.";
+} else {
+    echo "failed";
+}
 ?>
