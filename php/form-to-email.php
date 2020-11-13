27f1515d82f1cea600e4566b9a0a06f60ab28356
Andrew Lee <andrewimg@gmail.com>
2020-11-13 10:51:05 -0800

    php email form

---
+++ php/form-to-email.php
@@ -0,0 +1,23 @@
+<?php
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$subject = $_POST['subject'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_from = 'morningstaradvising.com';
+
+$email_subject = "Client Inquiry";
+
+$email_body = "You have received a new message from the user $firstName $lastName.\n".
+                "Here is the subject: $subject\n".
+                "Here is the message:\n $message";
+
+$to = "andrewimg@gmail.com";
+
+$headers = "From: $email_from \r\n";
+              
+$headers .= "Reply-To: $visitor_email \r\n";
+              
+mail($to,$email_subject,$email_body,$headers);
+?>
